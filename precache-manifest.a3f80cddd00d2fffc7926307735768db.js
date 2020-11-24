8b3e64111d4f56b4ae201aff410b5c727f1be705
khalil Dev <khalil.deve@gmail.com>
2020-11-24 08:37:37 +0800

    Updates

---
+++ precache-manifest.a3f80cddd00d2fffc7926307735768db.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f57131e529d0f3e65ba66ab57b26dc6",
+    "url": "/github.io/coronavirus-app/index.html"
+  },
+  {
+    "revision": "8da5835209d9530c88e2",
+    "url": "/github.io/coronavirus-app/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "96762a75769568731c73",
+    "url": "/github.io/coronavirus-app/static/css/main.980de7d0.chunk.css"
+  },
+  {
+    "revision": "8da5835209d9530c88e2",
+    "url": "/github.io/coronavirus-app/static/js/2.4d01ede2.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "/github.io/coronavirus-app/static/js/2.4d01ede2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96762a75769568731c73",
+    "url": "/github.io/coronavirus-app/static/js/main.9a35f099.chunk.js"
+  },
+  {
+    "revision": "1162099c07741550135a",
+    "url": "/github.io/coronavirus-app/static/js/runtime-main.cc836426.js"
+  },
+  {
+    "revision": "2c34cc69792daff5671bf029a30fe806",
+    "url": "/github.io/coronavirus-app/static/media/CoronaVirusHeader.2c34cc69.jpg"
+  },
+  {
+    "revision": "63eb7825050aefd0d9a5cbdf9d315ee8",
+    "url": "/github.io/coronavirus-app/static/media/loading.63eb7825.gif"
+  }
+]);
