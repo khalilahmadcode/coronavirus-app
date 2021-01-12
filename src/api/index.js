67d083f679a9de6cb554916c3db86f1db4311425
khalil Dev <khalil.deve@gmail.com>
2021-01-12 10:58:56 +0800

    CORS problem is solved

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = "https://covid19.mathdro.id/api"
+const url = "https://api.allorigins.win/raw?url=https://covid19.mathdro.id/api"
 
 export const fetchData = async (country) => {
     let changableUrl = url;     
